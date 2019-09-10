c4ec922779678338c335b59a4557d62bfa872b41
untaljacko <untaljacko@gmail.com>
2019-09-10 12:09:10 -0500

    fixed name of the folder

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last = number % 10
+
+if last > 5:
+    text = "and is greater than 5"
+elif last 
+    print("Last digit of {} is {}".format(number, last))
+ t("and is greater than 5"))
