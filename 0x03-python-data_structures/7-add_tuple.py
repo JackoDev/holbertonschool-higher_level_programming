e264732d2d95df430157978e6b0a38bb2bc18dc8
untaljacko <untaljacko@gmail.com>
2019-09-18 11:22:38 -0500

    task 7: fixed len of the tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if (len(tuple_b) < 1):
-        tuple_c = (tuple_a[0] + 0, tuple_a[1] + 0)
-    elif (len(tuple_b) < 2):
-        tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + 0)
-    else:
-        tuple_c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
-    return tuple_c
+    fst_tuple = tuple_a + (0, 0)
+    scd_tuple = tuple_b + (0, 0)
+    fst_tuple = fst_tuple[0:2]
+    scd_tuple = scd_tuple[0:2]
+    result = (fst_tuple[0] + scd_tuple[0], fst_tuple[1] + scd_tuple[1])
+    return result
