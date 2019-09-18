1713e5380bac7fbc4a6a3c1da5c11b348c70a522
untaljacko <untaljacko@gmail.com>
2019-09-18 12:15:55 -0500

    task 8: fixed case for None

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    f_char = sentence[0]
-    if (len(sentence) == 0 or sentence == ""):
-        f_char == "None"
+    f_char = ""
+    if (len(sentence) == 0):
+        f_char = None
+    else:
+        f_char = sentence[0]
     tuple_res = (len(sentence), f_char)
     return tuple_res
