297861f5da1d463c8b66b61c8a02229715535907
untaljacko <untaljacko@gmail.com>
2019-09-18 16:45:22 -0500

    task 2: fixed permissions to file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from sys import argv
-if __name__ == '__main__':
+if __name__ == "__main__":
     if len(argv) == 1:
         print("0 arguments.")
     else:
         if len(argv) == 2:
-            print(len(argv) - 1, "argument:")
+            print(len(argv)-1, "argument:")
         else:
-            print(len(argv) - 1, "arguments:")
+            print(len(argv)-1, "arguments:")
         i = 1
         while i < len(argv):
             print("{:d}:".format(i), argv[i])
