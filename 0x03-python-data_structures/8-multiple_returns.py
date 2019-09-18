6f02ff850a660fa29160a8ff9e0d1bfa258dcff0
untaljacko <untaljacko@gmail.com>
2019-09-18 11:44:41 -0500

    task: 8 tested

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    if sentence == "":
+        sentence[0] == "None"
+    f_char = sentence[0]
+    tuple_res = (len(sentence), f_char)
+    return tuple_res
