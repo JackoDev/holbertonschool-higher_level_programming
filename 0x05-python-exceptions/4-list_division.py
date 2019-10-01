c9b98a7b13e9e977f4c20dff49c80b66e5ae459f
untaljacko <untaljacko@gmail.com>
2019-09-30 22:47:08 -0500

    fixed pep8

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new = []
-    for i in range (list_lenght):
+    for i in range(list_lenght):
         try:
             total = 0
             total = my_list_1[i] / my_list_2[i]
