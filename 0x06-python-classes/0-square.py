20792e8acefb3d85d6505b2b335d4b7af9c1f060
untaljacko <untaljacko@gmail.com>
2019-10-01 18:28:03 -0500

    fixed documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+'''
+a simple declaration of a new class
+'''
+
+
 class Square:
-    """An empty class to defines a square"""
+    '''
+    An empty class to defines a square
+    '''
     pass
