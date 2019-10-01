0cf53d0f38ebd988acc2b05da31778f9123e0756
untaljacko <untaljacko@gmail.com>
2019-09-30 22:53:07 -0500

    fixed word length

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -3,7 +3,7 @@
 
 def list_division(my_list_1, my_list_2, list_length):
     new = []
-    for i in range(0, list_lenght):
+    for i in range(0, list_length):
         try:
             total = 0
             total = my_list_1[i] / my_list_2[i]
@@ -15,4 +15,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
         finally:
             new.append(total)
+
     return new
