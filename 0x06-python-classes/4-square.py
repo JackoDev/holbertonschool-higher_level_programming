76565d74a4d421a34ed81833d0d224310e7a91fa
untaljacko <untaljacko@gmail.com>
2019-10-01 22:04:56 -0500

    task 4: fixed order of statements

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''
+a simple declaration of a new class
+'''
+
+
+class Square:
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def area(self):
+        return (self.__size * self.__size)
