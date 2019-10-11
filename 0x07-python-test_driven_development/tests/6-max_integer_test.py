a118a564e30c0a620c044a220d7c91a3ae252193
untaljacko <untaljacko@gmail.com>
2019-10-11 16:09:48 -0500

    task 6: fixed pep8

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
 Unittest for the max_integer module
 """
@@ -8,19 +7,20 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
+
     """Test for empty list"""
     def test_empty(self):
         self.assertEqual(max_integer([]), None)
-       """Test for strings"""
+    """Test for strings"""
     def test_strings(self):
         self.assertEqual(max_integer(['H', 'o', 'l', 'a']), 'o')
-     """Test for same value"""
+    """Test for same value"""
     def test_samevalue(self):
         self.assertEqual(max_integer([7, 7, 7, 7]), 7)
-     """Test for no order list"""
+    """Test for no order list"""
     def test_no_order(self):
         self.assertEqual(max_integer([9, 6, 1, 8]), 9)
-      """Test for one negative"""
+    """Test for one negative"""
     def test_one_negative(self):
         self.assertEqual(max_integer([-1]), -1)
     """Test for just one number"""
