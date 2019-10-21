1e9f6a27304e711f25750bfaa6e656e37df61405
untaljacko <untaljacko@gmail.com>
2019-10-21 16:32:13 -0500

    fixed task 7 type of raise exceptions

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -15,6 +15,6 @@ class BaseGeometry():
     def integer_validator(self, name, value):
         """for validate the value"""
         if type(value) is not int:
-            raise Exception("{:s} must be an integer".format(name))
+            raise TypeError("{:s} must be an integer".format(name))
         if value <= 0:
-            raise Exception("{:s} must be greater than 0".format(name))
+            raise ValueError("{:s} must be greater than 0".format(name))
