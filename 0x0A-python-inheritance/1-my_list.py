a9339ef1fb76197ff46a3e2b9069f4a358e98f8b
untaljacko <untaljacko@gmail.com>
2019-10-21 11:41:40 -0500

    task 1: fixed identation

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+"""
+    prints the list, but sorted (ascending sort)
+"""
 
 
 class MyList(list):
-
+    """new class MyList that inherits from list"""
     def print_sorted(self):
-         print(sorted(self))
+        """print a list in ascending sort"""
+        print(sorted(self))
