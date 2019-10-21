0f3980c4f5ec681a1cf4b140ee82de238fbb2b4a
untaljacko <untaljacko@gmail.com>
2019-10-21 14:04:06 -0500

    task 2: fixed return

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -7,7 +7,4 @@ an instance of the specified class ; otherwise False.
 
 def is_same_class(obj, a_class):
     """Function to validate if a object is exactly an instance of the class"""
-    if isinstance(obj, a_class):
-        return True
-    else:
-        return False
+    return type(obj) == a_class
