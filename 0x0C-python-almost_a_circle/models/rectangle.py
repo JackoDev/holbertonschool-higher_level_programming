9a9089d33c54487b7f09ccfa599d1f09a7be0959
untaljacko <untaljacko@gmail.com>
2019-10-22 21:21:58 -0500

    task 6: _str_

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -74,3 +74,7 @@ class Rectangle(Base):
             for b in range(self.width):
                 print("#", end="")
             print()
+
+    def __str__(self):
+        return("[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height))
