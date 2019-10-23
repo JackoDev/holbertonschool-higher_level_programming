781f1ad4475ff1fb7d2e3d55100cd4719420be34
untaljacko <untaljacko@gmail.com>
2019-10-23 11:42:59 -0500

    task 0: fixed 1 character extra in the output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """for read and write a file"""
     with open(filename, encoding='utf-8') as a_file:
-        print(a_file.read())
+        print(a_file.read(), end="")
