55899322b57427b800ba0ae12587d3ea969b2afd
untaljacko <untaljacko@gmail.com>
2019-10-22 22:08:06 -0500

    task 10: square class basics, fixed super class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,7 +5,8 @@ from models.rectangle import Rectangle
 
 class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
+        self.size = size
 
     @property
     def size(self):
