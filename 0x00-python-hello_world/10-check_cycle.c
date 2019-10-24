4ebbcf75c854236afe936d3bbd8a921ec5763051
untaljacko <untaljacko@gmail.com>
2019-10-24 09:00:28 -0500

    adding conditional for empty list

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -10,7 +10,7 @@ int check_cycle(listint_t *list)
 
 	pointer1 = list;
 	pointer2 = pointer1->next;
-	if (list == NULL)
+	if (list == NULL or list->next == NULL)
 		return (0);
 	while (pointer2 != NULL)
 	{
