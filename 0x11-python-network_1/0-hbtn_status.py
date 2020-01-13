4ad3a835c63196f5ecfbc54a8ab0b324e6ead597
untaljacko <untaljacko@gmail.com>
2020-01-13 16:34:25 +0000

    [fix] task 0, adding main to avoid execution

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """ a Python script that fetches https://intranet.hbtn.io/status """
-
-
 from urllib import request
 
 
-with request.urlopen('https://intranet.hbtn.io/status') as req:
-    html = req.read()
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(html))
-    print("\t- utf8 content: {}".format(html.decode("utf-8")))
+if __name__=="__main__":
+    with request.urlopen('https://intranet.hbtn.io/status') as req:
+        html = req.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode("utf-8")))
