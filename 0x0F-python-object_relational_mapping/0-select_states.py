1cb64f8d01a3fd8816d60a607df66fe00a5bbf6c
untaljacko <untaljacko@gmail.com>
2020-01-27 22:18:54 +0000

    [fix] database syntaxis for myqldb

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,8 +5,8 @@ from sys import argv
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=argv[1], passwd=argv[2], port=3306,
-                         host='localhost', db=argv[3])
+    db = MySQLdb.connect(host='localhost', user=argv[1], passwd=argv[2],
+                         db=argv[3])
     cursor = db.cursor()
     cursor.execute('SELECT * FROM states ORDER BY state.id')
     for state in cursor.fetchall():
