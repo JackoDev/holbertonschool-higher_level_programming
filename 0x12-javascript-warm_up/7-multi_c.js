d2c6b46a2cd6067480c0496535aa13d8b52cdf47
JackoDev <untaljacko@gmail.com>
2020-02-04 19:55:19 -0500

    [new] task 7

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a script that prints x times “C is fun”
+const c = 'C is fun';
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(c);
+  }
+}
