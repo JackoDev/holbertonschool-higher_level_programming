0385d8728499b8488ff8a663238d75eeafe45b12
JackoDev <untaljacko@gmail.com>
2020-02-04 19:52:10 -0500

    [new] task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// a script that prints 3 lines using an array of strings and a loop
+const lines = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(lines[i]);
+}
