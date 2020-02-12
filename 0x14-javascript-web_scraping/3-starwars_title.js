764c5e69576e80ae2a5bc7e721aabec44e4f4982
JackoDev <untaljacko@gmail.com>
2020-02-11 20:05:31 -0500

    [new] add task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// doc for task 3
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(JSON.parse(body).title);
+  }
+});
